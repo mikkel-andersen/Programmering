175d200a9f5c790feb71267c9763a917e69f7a1b
mikkel <mikkel.andersen2810@gmail.com>
2023-03-15 16:25:46 +0100

    Opdaterede system out print

--- L08 - Opgaver/src/Main.java
+++ L08 - Opgaver/src/Main.java
@@ -4,8 +4,8 @@ public class Main {
         a1.setAnnualInterestRate(4.5);
         a1.withdraw(2500);
         a1.deposit(3000);
-        System.out.println(a1.getBalance());
-        System.out.println(a1.getMonthlyInterest());
+        System.out.println("Balance is: " + a1.getBalance());
+        System.out.println("Monthly interest is: " + a1.getMonthlyInterest());
         System.out.println("Account created: " + a1.getDateCreated());
     }
 }
