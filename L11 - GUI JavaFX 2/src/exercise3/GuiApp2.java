e7caf1ce1154273af94ebe3118ea63b801af4906
mikkel <mikkel.andersen2810@gmail.com>
2023-04-11 15:01:07 +0200

    JavaFX gui opgaver (11. april) Opgave 2 og klargøring til opgave 3

---
+++ L11 - GUI JavaFX 2/src/exercise3/GuiApp2.java
@@ -0,0 +1,7 @@
+package exercise3;
+
+public class GuiApp2 {
+    public static void main(String[] args) {
+        GuiExercise3.launch(GuiExercise3.class);
+    }
+}
---
+++ L11 - GUI JavaFX 2/src/exercise3/GuiExercise3.java
@@ -0,0 +1,123 @@
+package exercise3;
+
+import Exercise1.Person;
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+public class GuiExercise3 extends Application {
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("ListView Demo2");
+        GridPane pane = new GridPane();
+        this.initContent(pane);
+
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    // -------------------------------------------------------------------------
+
+    private final TextField txfName = new TextField();
+    private final TextField txfTitle = new TextField();
+    private final CheckBox checkbox = new CheckBox();
+    private final ListView<String> lvwNames = new ListView<>();
+    private final ArrayList<String> persons = new ArrayList<>();
+
+    private void initContent(GridPane pane) {
+        // show or hide grid lines
+        pane.setGridLinesVisible(false);
+        // set padding of the pane
+        pane.setPadding(new Insets(20));
+        // set horizontal gap between components
+        pane.setHgap(10);
+        // set vertical gap between components
+        pane.setVgap(10);
+
+        // add a label to the pane (at col=0, row=0)
+        Label lblName = new Label("Name:");
+        pane.add(lblName, 0, 0);
+        Label lblTitle = new Label("Title:");
+        pane.add(lblTitle, 0, 1);
+        Label lblSenior = new Label("Senior");
+        pane.add(lblSenior, 0, 2);
+
+        // add a label to the pane (at col=0, row=1)
+        Label lblNames = new Label("Names:");
+        pane.add(lblNames, 0, 3);
+        GridPane.setValignment(lblNames, VPos.TOP);
+
+        // add a text field to the pane (at col=1, row=0)
+        pane.add(txfName, 1, 0);
+        pane.add(txfTitle, 1, 1);
+        pane.add(checkbox, 1, 2);
+
+
+        // add a listView to the pane(at col=1, row=1)
+        pane.add(lvwNames, 1, 3);
+        lvwNames.setPrefWidth(200);
+        lvwNames.setPrefHeight(200);
+
+        ChangeListener<String> listener = (ov, oldString, newString) -> this.selectionChanged();
+        lvwNames.getSelectionModel().selectedItemProperty().addListener(listener);
+
+        lvwNames.getSelectionModel().clearSelection();
+
+        // add a button to the pane (at col=4, row=0)
+        Button btnAdd = new Button("Add");
+        pane.add(btnAdd, 2, 3);
+        // btnAdd.setDefaultButton(true);
+
+        // connect a method to the button
+        btnAdd.setOnAction(event -> this.addAction());
+
+    }
+
+    // -------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
+    // Selection changed actions
+
+    private void selectionChanged() {
+        String selected = lvwNames.getSelectionModel().getSelectedItem();
+        if (selected != null) {
+            txfName.setText(selected);
+        } else {
+            txfName.clear();
+        }
+    }
+
+    // -------------------------------------------------------------------------
+    // Button actions
+
+    private void addAction() {
+        String name = txfName.getText();
+        String title = txfTitle.getText();
+        if (name.length() <= 0 || title.length() <= 0) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Add person");
+            alert.setHeaderText("Name or title missing");
+            alert.setContentText("Type the name or title of the person");
+            alert.show();
+        } else {
+            boolean senior;
+            if (checkbox.isSelected()) {
+                senior = true;
+            } else {
+                senior = false;
+            }
+            Exercise1.Person p = new Person(name, title, senior);
+
+            persons.add(p.toString());
+            lvwNames.getItems().setAll(persons);
+        }
+    }
+
+}
